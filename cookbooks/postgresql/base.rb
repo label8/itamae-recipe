2a6cb2d7ac21e5309d92d3a063bc8565646e322f
Tsunemasa Hachiya <label8.me@gmail.com>
2016-09-21 20:18:07 +0900

    Add postgres role in 'postgresql/base.rb', Merge node attribute, Change 'entry.rb'

--- cookbooks/postgresql/base.rb
+++ cookbooks/postgresql/base.rb
@@ -15,26 +15,30 @@ end
 
 package "postgresql"
 
-template node['pgsql']['hba_conf'] do
-  source "#{node['pathes']['cookbooks_root']}/postgresql/templates#{node['pgsql']['hba_conf']}.erb"
-  variables(
-    address: node['pgsql']['values']['hba']['address'],
-    method: node['pgsql']['values']['hba']['method']
-  )
+remote_file node['pgsql']['hba_conf'] do
+  source "#{node['pathes']['cookbooks_root']}/postgresql/remote_files#{node['pgsql']['hba_conf']}"
+  mode "644"
   owner "postgres"
   group "postgres"
 end
 
-template node['pgsql']['postgresql_conf'] do
-  source "#{node['pathes']['cookbooks_root']}/postgresql/templates#{node['pgsql']['postgresql_conf']}.erb"
-  variables(
-    lc_messages: node['pgsql']['values']['global']['lc_messages'],
-    lc_monetary: node['pgsql']['values']['global']['lc_monetary'],
-    lc_numeric: node['pgsql']['values']['global']['lc_numeric'],
-    lc_time: node['pgsql']['values']['global']['lc_time']
-  )
+remote_file node['pgsql']['postgresql_conf'] do
+  source "#{node['pathes']['cookbooks_root']}/postgresql/remote_files#{node['pgsql']['postgresql_conf']}"
+  mode "644"
   owner "postgres"
   group "postgres"
+end
+
+execute 'create user' do
+  user_exists = <<-EOL
+    sudo -u postgres -i sh -c "psql -c \\"SELECT * FROM pg_user WHERE usename='#{node['pgsql']['user']}'\\"" | grep #{node['pgsql']['user']}
+  EOL
+
+  command <<-EOL
+    sudo -u postgres -i sh -c "psql -c \\"CREATE ROLE #{node['pgsql']['user']} LOGIN CREATEDB PASSWORD '#{node['pgsql']['password']}'\\""
+  EOL
+
+  not_if user_exists
 end
 
 service "postgresql" do
--- entry.rb
+++ entry.rb
@@ -1,4 +1,16 @@
-node['roles'] = node['roles'] || []
-node['roles'].each do |role|
-  include_recipe "roles/#{role}/base.rb"
+# itamae ssh で指定したIPアドレスの第四オクテットを見て
+# どのロールを読み込むか決める
+matches = ARGV[2].match(/.+\.(\d+)$/)
+fourth_octet = matches[1]
+
+case fourth_octet.to_i
+when 20
+  include_recipe "roles/front/base.rb"
+when 21
+  include_recipe "roles/web/base.rb"
+  include_recipe "roles/app/base.rb"
+when 40
+  include_recipe "roles/db/base.rb"
+else
+  p "192.168.1.[20:front, 21-39:web, 40:db]"
 end
