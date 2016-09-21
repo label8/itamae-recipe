e40913e8c0a009a73909d9d01d003282239e8639
Tsunemasa Hachiya <label8.me@gmail.com>
2016-09-22 01:08:12 +0900

    Create unicorn script

--- cookbooks/rails/unicorn.rb
+++ cookbooks/rails/unicorn.rb
@@ -14,3 +14,20 @@ template node['rails']['unicorn']['file'] do
   owner "vagrant"
   group "vagrant"
 end
+
+template node['rails']['unicorn']['script'] do
+  source "#{node['pathes']['cookbooks_root']}/rails/templates#{node['rails']['unicorn']['script']}.erb"
+  mode "755"
+  owner "root"
+  group "root"
+end
+
+execute "Load unicorn service" do
+  user "root"
+  command "systemctl daemon-reload"
+  only_if "test -f #{node['rails']['unicorn']['script']}"
+end
+
+service "unicorn" do
+  action [:enable, :start]
+end
